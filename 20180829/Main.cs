eff5657a675dcda476e6bbe045713b597a45d272
alvin125 <48213834+alvin125@users.noreply.github.com>
2020-01-16 23:14:58 -0500

    dd

--- 20180829/Main.cs
+++ 20180829/Main.cs
@@ -240,7 +240,7 @@ namespace _20180829
         //공지사항 
         private void SetHeadLine()
         {
-            for (int i = 0; i < Login.BoardList.Count; i++)
+            for (int i = Login.BoardList.Count - 1; i > 0; i--)
             {
                 if(Login.BoardList[i].Category == "공지사항" && i == (Login.BoardList.Count-1))
                 {
